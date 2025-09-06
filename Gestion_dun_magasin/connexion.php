01c6b4e5a2d163e974e87a932f0ae913b1f90e43
FEKOTAN Moustapha <129105520+MoustaphaFEKOTAN@users.noreply.github.com>
2025-09-06 15:56:30 +0100

    Delete Gestion_dun_magasin directory

--- Gestion_dun_magasin/connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Connexion à la base de données
-$nom_serveur = "localhost";
-$utilisateur = "root";
-$mot_de_passe = "";
-$nom_base_donnees = "gestion_dun_magasin";
-$con = mysqli_connect($nom_serveur, $utilisateur, $mot_de_passe, $nom_base_donnees);
-?>
